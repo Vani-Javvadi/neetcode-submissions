87c52306062fe68dfae6a42117b8a41b39c7e212
Vani-Javvadi <104265093+Vani-Javvadi@users.noreply.github.com>
2026-06-22 01:56:08 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // DLQ of size K 
+        // the frontend element will hold maximum value in window 
+        // once we move out of the window , we will remove the front , if front is part of window 
+        Deque<Integer> dlq = new ArrayDeque<>();
+        int n = nums.length;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            while(!dlq.isEmpty() && nums[dlq.peekLast()] < nums[i]) {
+                dlq.pollLast();
+            }
+            dlq.offerLast(i);
+            if(dlq.peekFirst() <= i-k) {
+                dlq.pollFirst();
+            }
+            // add front element to list 
+            if(i >= k-1) {
+                list.add(nums[dlq.peekFirst()]);
+            } 
+        }
+        int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
+        return arr;
+    }
+}
