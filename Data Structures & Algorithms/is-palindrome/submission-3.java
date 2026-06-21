c28e54436dca661080ca7d874410590ac238e4e2
Vani-Javvadi <104265093+Vani-Javvadi@users.noreply.github.com>
2026-06-21 17:07:20 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int p1 = 0, p2 = n - 1;
+
+        while (p1 < p2) {
+            Character l = s.charAt(p1);
+            Character r = s.charAt(p2);
+            if (Character.toLowerCase(l) ==  Character.toLowerCase(r)) {
+                p1++;
+                p2--;
+            } else if(!Character.isLetterOrDigit(l)) {
+                p1++;
+            } else if(!Character.isLetterOrDigit(r)){
+                p2--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
