3d8ea82f28e1e8d1c74c1bbbfa788de06120b029
Vani-Javvadi <104265093+Vani-Javvadi@users.noreply.github.com>
2026-06-23 22:36:07 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // BF - n^2 , we will find first highest in right part 
+
+        // optimal - next greater element 
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+        // we will come from end of array 
+        for(int i=n-1; i>=0; i--) {
+            if(st.isEmpty()) {
+                result[i] = 0;
+            } else {
+                // if previous temps are less than current one , they will no longer be warmer than current one 
+                // so no point in having them so pop 
+                while(!st.isEmpty() &&  temperatures[st.peek()] <= temperatures[i]) {
+                    st.pop();
+                }
+                result[i] = !st.isEmpty() ? st.peek() - i : 0;
+            }
+            st.push(i);
+        }
+        return result;
+    }
+}
